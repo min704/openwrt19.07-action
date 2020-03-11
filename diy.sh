8f56b83b365ac1f345b676639485f47d66cc2cdb
min704 <46183464+min704@users.noreply.github.com>
2020-03-12 01:47:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 #添加额外软件包
-git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 ./scripts/feeds update -a
